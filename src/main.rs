298eade7790fff325f65cc8b588aa005edbf640b
Viktor Poletansky <poletansky@gmail.com>
2021-07-07 21:47:49 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use std::collections::{VecDeque, HashMap};
-use std::convert::TryFrom;
-
 #[derive(Clone, PartialEq, Eq, Hash, Copy, Debug)]
 enum LaserSource {Down, Right}
 
@@ -64,16 +61,18 @@ impl MapMeta {
             player_y: player_pos.1,
         };
         objects.iter()
-            .for_each(|(obj, poss)|
-                poss.iter().for_each(|p| {
-                    mp[res.from_pair(p)] = *obj
+            .for_each(|(obj, poss)| poss.iter()
+                .map(|p| res.from_pair(p))
+                .for_each(|p| {
+                    mp[p] = *obj
                 })
         );
         mp[res.from_pair(&player_pos)] = Cell::Player;
         (res, mp)
     }
 
-    fn from_pair<I: Copy>(&self, (x, y): &(I, I)) -> usize where usize: TryFrom<I> {
+    fn from_pair<I: Copy>(&self, (x, y): &(I, I)) -> usize where usize: std::convert::TryFrom<I> {
+        use std::convert::TryFrom;
         let (ix, iy) = (usize::try_from(*x), usize::try_from(*y));
         match ix {
             Ok(ix) => match iy {
@@ -122,44 +121,47 @@ impl MapMeta {
     }
 
     fn draw_lasers(&self, mp: &mut MapRepr) {
-        let mut to_update: Vec<usize> = Vec::new();
         for (i, (dx, dy, d)) in self.laser_poss.iter() {
-            let (mut kx, mut ky, mut k) = ((i % self.width) as isize, (i / self.width) as isize, *i as isize);
-            while !self.not_in_map((kx, ky)) {
-                match mp[k as usize] {
+            let (k0, times) = {
+                let (kx, ky) = ((i % self.width) as isize, (i / self.width) as isize);
+                (*i as isize, if *dx != 0 {
+                    if *dx < 0 {kx + 1} else {self.width as isize - kx + 1}
+                } else {
+                    if *dy < 0 {ky + 1} else {self.height as isize - ky + 1}
+                })
+            };
+            for k in (0..times).into_iter()
+                .map(|x| k0 + d * x)
+                .map(|k| k as usize) {
+                match unsafe {mp.get_unchecked(k)} {
                     Cell::LaserSource(_) | Cell::Laser => {}
-                    Cell::Space => {to_update.push(k as usize)}
+                    Cell::Space => {unsafe {*mp.get_unchecked_mut(k) = Cell::Laser}}
                     _ => {break;}
                 };
-                k += d;
-                kx += dx;
-                ky += dy;
             }
         }
-        to_update.iter().for_each(|k| mp[*k] = Cell::Laser)
     }
 
     fn bfs(&self, max_turns: u32, v_start: (isize, isize), mp: MapRepr) -> Option<Vec<MapRepr>> {
-        let mut visited = HashMap::new(); // from
-        let mut queue: VecDeque<(MapRepr, (usize, isize, isize), MapRepr, u32)> = VecDeque::new();
+        let mut from = std::collections::HashMap::new();
+        let mut queue = std::collections::VecDeque::new();
         queue.push_back((mp.clone(), (self.from_pair(&v_start), v_start.0, v_start.1), mp, 0));
         while !queue.is_empty() {
             let (strmap, (vi, v_x, v_y), p, sz) = queue.pop_front().unwrap();
-            if visited.contains_key(&strmap) || self.is_player_dead(&strmap, &(v_x, v_y)) {
+            if from.contains_key(&strmap) || self.is_player_dead(&strmap, &(v_x, v_y)) {
                 continue
             }
             if !strmap.contains(&Cell::LeverOff) {
-                println!("Found solution in {} steps", sz);
-                visited.insert(strmap.clone(), p);
+                from.insert(strmap.clone(), p);
                 let mut res = Vec::new();
                 let mut cur = &strmap;
-                while *cur != visited[cur] {
+                while *cur != from[cur] {
                     res.push(cur.clone());
-                    cur = &visited[cur];
+                    cur = &from[cur];
                 }
                 return Some(res)
             }
-            visited.insert(strmap.clone(), p.clone());
+            from.insert(strmap.clone(), p.clone());
             for (dx, dy, dir) in [(0, 1), (0, -1), (1, 0), (-1, 0)].iter()
                 .map(|(x, y)| (x, y, y * self.width as isize + x)) {
                 let (to_x, to_y) = (v_x + dx, v_y + dy);
@@ -195,14 +197,16 @@ impl MapMeta {
                                     _ => *c
                                 };
                             }
-                            replace(&mut tmp, to, Cell::Space, after_to, Cell::Cube);
+                            tmp[to] = Cell::Space;
+                            tmp[after_to] = Cell::Cube;
                             tmp
                         };
                         (strmap_new, (vi, v_x, v_y))
                     }
                     Cell::Space => {
                         let mut strmap_new = strmap.clone();
-                        replace(&mut strmap_new, vi, Cell::Space, to, Cell::Player);
+                        strmap_new[vi] = Cell::Space;
+                        strmap_new[to] = Cell::Player;
                         (strmap_new, (to, to_x, to_y))
                     }
                     Cell::LeverOff => {
@@ -221,10 +225,34 @@ impl MapMeta {
     }
 }
 
-fn replace(v: &mut MapRepr, p1: usize, c1: Cell, p2: usize, c2: Cell) {
-    v[p1] = c1;
-    v[p2] = c2;
-}
+// #[bench]
+// fn benchmark_bfs(b: &mut test::Bencher) {
+// fn benchmark_bfs() {
+//     let player_pos = (7, 0);
+//     let max_turns = 49;
+//     let (map_meta, map_repr) = MapMeta::from(&[
+//         (Cell::Cube, &[(1, 3), (5, 2), (7, 1), (7, 2), (7, 3)][..]),
+//         (Cell::Wall, &[(0, 1), (1, 1), (2, 0), (3, 0), (4, 0), (5, 0), (8, 4), (7, 4), (7, 5), (1, 5), (0, 4)][..]),
+//         (Cell::LaserSource(LaserSource::Down), &[(2, 5), (3, 5), (6, 5)][..]),
+//         (Cell::LaserSource(LaserSource::Right), &[(3, 4)][..]),
+//         (Cell::LeverOff, &[(1, 4), (5, 5)][..]),
+//     ][..], player_pos);
+//     (0..30).for_each(|c| {
+//         map_meta.bfs(max_turns, (player_pos.0 as isize, player_pos.1 as isize), map_repr);
+//     });
+// }
+// fn run_benchmark(d: MapMeta, a: u32, b: (isize, isize), c: MapRepr) -> Vec<Vec<MapRepr>> {
+//     let n = 30;
+//     let mut res = Vec::with_capacity(n);
+//     let mut times = Vec::with_capacity(n);
+//     for _ in 0..n {
+//         let start = std::time::Instant::now();
+//         res.push(d.bfs(a, b, c.clone()).unwrap());
+//         times.push(start.elapsed().as_millis());
+//     }
+//     println!("{}", times.into_iter().min().unwrap());
+//     res
+// }
 
 fn main() {
     let player_pos = (7, 0);
@@ -236,13 +264,16 @@ fn main() {
         (Cell::LaserSource(LaserSource::Right), &[(3, 4)][..]),
         (Cell::LeverOff, &[(1, 4), (5, 5)][..]),
     ][..], player_pos);
-
+    // run_benchmark(map_meta.clone(), max_turns, (player_pos.0 as isize, player_pos.1 as isize), map_repr.clone());
     println!("Starting position:\n{}", map_meta.fmt(&map_repr));
     match map_meta.bfs(max_turns, (player_pos.0 as isize, player_pos.1 as isize), map_repr) {
-        Some(solution) => solution.iter().rev().for_each(|state| {
-            println!("=======================");
-            print!("{}", map_meta.fmt(state));
-        }),
+        Some(solution) => {
+            println!("Found solution in {} steps", solution.len());
+            solution.iter().rev().map(|x| map_meta.fmt(x)).for_each(|state| {
+                println!("=======================");
+                print!("{}", state);
+            })
+        }
         None => println!("Solution was not found :("),
     }
 }
